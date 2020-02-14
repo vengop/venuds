332850bf9541ef113415a2aeaa06c1b5bf12b5b0
vengop <datascit@gmail.com>
2020-02-14 15:40:27 +0530

    chatfile commited

---
+++ nitchatmod.py
@@ -0,0 +1,69 @@
+import nltk
+import numpy as np
+import random
+import string
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+f=open('d:/student/chatbot.txt','r',errors = 'ignore')
+
+raw=f.read()
+
+raw=raw.lower()# converts to lowercase
+
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+lemmer = nltk.stem.WordNetLemmatizer()
+#WordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+def greeting(sentence):
+ 
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    #word_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize)
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+flag=True
+print("Agent: My name is NIT. I will answer your queries about NITW course. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("Agent: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("Agent: "+greeting(user_response))
+            else:
+                print("Agent: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+                
+    else:
+        flag=False
+        print("Agent: Bye! take care..")
+
+
+    
